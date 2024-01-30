245dea5225778556bc631c3ab3e4baa43378b485
Rana-Paul <ranapaul741223@gmail.com>
2024-01-30 08:23:28 +0000

    auth error

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,31 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-export const {handlers: {GET, POST}, auth, signIn, signOut} = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-})
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({ token, account }) {
+      if (token) {
+        token.access_token = account?.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.user.access_token = token.access_token;
+      return session;
+    },
+  },
+});
